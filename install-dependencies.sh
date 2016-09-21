466d56a0712a813d928d20b0bc765f5552ee4039
Luka Stopar <luka.stopar@ijs.si>
2016-09-21 12:59:05 +0200

    additional flags for dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y cmake
-sudo apt-get install -y python-dev
-sudo apt-get install -y libboost-all-dev
-sudo apt-get install -y clang
-sudo apt-get install -y libclang-dev
+
+sudo apt-get install -y $@ cmake
+sudo apt-get install -y $@ python-dev
+sudo apt-get install -y $@ libboost-all-dev
+sudo apt-get install -y $@ clang
+sudo apt-get install -y $@ libclang-dev
