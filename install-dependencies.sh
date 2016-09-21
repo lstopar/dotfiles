b4251a61dfe83e1ddb0b2c5dcccd5cb1ac8d12dc
Luka Stopar <luka.stopar@ijs.si>
2016-09-21 12:56:30 +0200

    dependencies in separate file

---
+++ install-dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install -y cmake
+sudo apt-get install -y python-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y clang
+sudo apt-get install -y libclang-dev
