29035aee60ed5652cc31b241cae6a28ab535c02b
Luka Stopar <luka.stopar@ijs.si>
2016-11-01 18:13:12 +0100

    added credentials caching for git

--- git/configure.sh
+++ git/configure.sh
@@ -2,6 +2,8 @@
 
 # configure git to autocorrect your typos
 git config --global help.autocorrect 30
+# cache credentials for 15 minutes once entered
+git config --global credentail.helper cache
 
 # set colors to git status
 git config --global color.status.added 'green'
