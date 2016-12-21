ffc8aba5bcc129f7f3af85e3649dd6b84a555f66
Luka Stopar <luka.stopar@ijs.si>
2016-12-21 21:18:48 +0100

    ...

--- bash/scripts/setup.sh
+++ bash/scripts/setup.sh
@@ -2,7 +2,7 @@
 
 echo 'setting up bash scripts ...'
 
-if [ -d ~/bin ]; then
+if [ -d ~/bin ] && [ ! -L ~/bin ]; then
     echo '~/bin is a directory, should delete manually, SKIPPING!!'
     exit 0
 fi
