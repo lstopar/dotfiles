f99b56eea063f96ff688939cc1d945798ab0eea5
Luka Stopar <luka.stopar@ijs.si>
2017-11-22 18:52:22 +0100

    added mac specific configuration for ssh

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-if [ -e "$HOME/.vimrc" ] || [ -L "$HOME/.vimrc" ]; then 
+if [ -e "$HOME/.ssh/config" ] || [ -L "$HOME/.ssh/config" ]; then 
     echo 'removing old ~/.ssh/config'
     rm ~/.ssh/config
+fi
+if [ -e "$HOME/.ssh/os-specific" ] || [ -L "$HOME/.ssh/os-specific" ]; then 
+    echo 'removing old ~/.ssh/os-specific'
+    rm ~/.ssh/config
+fi
+
+os_file='linux-specific'
+if [ `uname -s` == "Darwin\n" ]; then
+    os_file='mac-specific'
 fi
 
 echo 'linking configuration file'
 ln -s `pwd`/config ~/.ssh/config
+ln -s `pwd`/$os_file ~/.ssh/os-specific
